3575818330dbc2ac70bb5814ebbccaabd7854146
itaysabato <itaysabato@gmail.com>
2016-12-23 21:49:20 +0100

    First push.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# cancelable-awaiter
-A replacement for Typescript's default awaiter helper that supports Bluebird promise cancelations.
+# Cancelable Awaiter
+A replacement for Typescript's default awaiter helper that supports Bluebird promise cancellations.
+
+## Installation
+`npm install --save cancelable-awaiter`
+
+## Usage
+Depends on [bluebird](https://www.npmjs.com/package/bluebird) being installed as a dependency and configured to support [cancellations](http://bluebirdjs.com/docs/api/cancellation.html):
+```typescript
+import * as tslib from "tslib";
+import * as Bluebird from "bluebird";
+import * as awaiter from "cancelable-awaiter";
+
+Bluebird.config({
+    cancellation: true
+});
+
+(tslib as any).__awaiter = awaiter;
+```
+
+Then async/await syntax can be used in conjunction with promise cancellations:
+
+```typescript
+async function doSomeThings() {
+    await doFirstThing()
+        .then(() => console.log("Done first thing!"))
+        // will be invoked anyway:
+        .finally(() => console.log("May have done first thing."));
+    
+    await doSecondThing()
+        .then(() => console.log("Done second thing!"));
+}
+
+const promise = doSomeThings()
+    .finally(() => console.log("May have done some things."));
+
+Promise.delay(1000)
+    .then(() => promise.cancel());
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+var Bluebird = require("bluebird");
+
+exports = function (thisArg, _arguments, P, generator) {
+    // always use Bluebird:
+    P = Bluebird;
+
+    // keep track of the currently awaited promise:
+    var currentPromise = P.resolve();
+
+    return new P(function (resolve, reject, onCancel) {
+        // The currently awaited promise is also canceled:
+        onCancel(function () {currentPromise.cancel()});
+
+        function step(result) {
+            if (currentPromise.isCancelled()) {
+                return;
+            }
+
+            currentPromise = P.resolve(result.value);
+
+            if (result.done) {
+                resolve(currentPromise);
+            }
+            else {
+                currentPromise.then(fulfilled, rejected);
+            }
+        }
+
+        // Rest is copied from original awaiter:
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    })
+        // This ensures any consumer lastly-s will be invoked only after the last awaited promise's lastly is resolved:
+        .lastly(function () {return currentPromise.reflect()});
+};
